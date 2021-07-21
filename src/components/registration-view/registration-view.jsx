f1900177e7680b8141a1a210439406c6ea1ec638
Klaus Wehner <74401112+KlausWehner@users.noreply.github.com>
2021-07-21 14:40:43 +0200

    some updates

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -9,4 +9,60 @@ export function RegistrationView(props) {
   const [ Email, setEmail ] = useState('');
   const [ Birthday, setBirthday ] = useState('');
 
+
+  const handleSubmit = () => {
+    e.preventDefault();
+    console.log(username, password, email, birthday);
+  };
+
+  return (
+    <form>
+      <label>
+        Your Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <label>
+        Set your Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <label>
+        Your email address:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+
+      <label>
+        Your birthday (optional):
+        <input
+          type="birthday"
+          value={birthday}
+          onChange={(e) => setBirthday(e.target.value)}
+        />
+      </label>
+      
+      <button type="submit" onClick={handleSubmit}>
+        Submit
+      </button>
+    </form>
+  );
+
+
+
+
+
+
+
+
+
 }
