6eca93abf7fc4b98567ac73439befaa11d31bbfd
Klaus Wehner <74401112+KlausWehner@users.noreply.github.com>
2021-07-23 16:39:06 +0200

    updates

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -15,7 +15,9 @@ export class MovieCard extends React.Component {
         <Card.Body>
           <Card.Title>{movie.Title}</Card.Title>
           <Card.Text>{movie.Description}</Card.Text>
-          <Button onClick={() => onMovieClick(movie)} variant="link">MORE INFO</Button>
+          <div className="d-grid gap-2">
+            <Button variant="outline-dark" size="lg" onClick={() => onMovieClick(movie)}> -  MORE INFO - </Button></div>
+            {/* That should make a full width button? */}
         </Card.Body>
       </Card>
     );
